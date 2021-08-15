5ba50c8268d4fb28b14afeb389a8a9792c72f5d3
Antoni Cano <antoni.cano96@gmai.com>
2021-08-15 12:53:35 +0200

    rutas y limpieza app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { UnitsService } from './services/units.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  title = 'ogame';
+export class AppComponent {
 
   constructor(
-    private unitService:UnitsService
   ){}
-
-  ngOnInit(){
-    this.getUnits();
-  }
-
-  getUnits(){
-   this.unitService.getUnits().subscribe(data => {
-     console.log(data);
-   })
-  }
-
 }
